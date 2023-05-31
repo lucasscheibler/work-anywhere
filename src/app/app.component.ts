538f35a686791d6e3045a79b7681b2b510ddce57
lucasscheibler <lucasscheibler@gmail.com>
2023-05-31 08:39:15 -0300

    feat: initial home page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'work-anywhere';
+  bgImageVariable: string = "";
+  randomNumber: number = 0;
+
+  async ngOnInit() {
+    let classCycle = [
+      "assets/img/hero-bg1.jfif",
+      "assets/img/hero-bg2.jpeg",
+      "assets/img/hero-bg3.jpg",
+      "assets/img/hero-bg4.jpeg",
+    ];
+    this.randomNumber = Math.floor(Math.random() * classCycle.length);
+    this.bgImageVariable = classCycle[this.randomNumber];
+  }
+  // Background image rotation
+  
+
+
 }
