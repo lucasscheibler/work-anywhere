a8a58c15740468b9321961e866ec16ddfc68ef14
lucasscheibler <lucasscheibler@gmail.com>
2023-05-31 17:48:21 -0300

    feat: updated home page background image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'work-anywhere';
-  bgImageVariable: string = "";
-  randomNumber: number = 0;
 
   async ngOnInit() {
-    let classCycle = [
-      "assets/img/hero-bg1.jfif",
-      "assets/img/hero-bg2.jpeg",
-      "assets/img/hero-bg3.jpg",
-      "assets/img/hero-bg4.jpeg",
-    ];
-    this.randomNumber = Math.floor(Math.random() * classCycle.length);
-    this.bgImageVariable = classCycle[this.randomNumber];
+
   }
-  // Background image rotation
-  
 
 
 }
