24a6e8d27ceb8e14a2345bc3114b5504ecf9b95d
lucasscheibler <lucasscheibler@gmail.com>
2023-06-02 08:38:29 -0300

    feat: initial homepage layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   title = 'work-anywhere';
 
+  mockData = [
+    {
+      "image": "https://via.placeholder.com/150",
+      "title": "Workplace 1",
+      "description": "Workplace 1 description"
+    },
+    {
+      "image": "https://via.placeholder.com/150",
+      "title": "Workplace 2",
+      "description": "Workplace 2 description"
+    },
+    {
+      "image": "https://via.placeholder.com/150",
+      "title": "Workplace 3",
+      "description": "Workplace 3 description"
+    },
+    {
+      "image": "https://via.placeholder.com/150",
+      "title": "Workplace 4",
+      "description": "Workplace 4 description"
+    }
+  ]
+
   async ngOnInit() {
 
   }
